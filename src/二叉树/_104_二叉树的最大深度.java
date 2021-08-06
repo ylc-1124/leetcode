b1ff5a9f4a97d568080864d44f3f819530dbbbe0
ylc-1124 <853703920@qq.com>
2021-08-06 14:41:05 +0800

    提交题目

--- src/二叉树/_104_二叉树的最大深度.java
+++ src/二叉树/_104_二叉树的最大深度.java
@@ -8,13 +8,15 @@ import java.util.Queue;
  */
 public class _104_二叉树的最大深度 {
     //方法一：递归
-//    public int maxDepth(TreeNode root) {
-//        if (root==null) return 0;
-//        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-//    }
+    public int maxDepth(TreeNode root) {
+        if (root==null) return 0;
+        int left = maxDepth(root.left); //递归左子树,返回当前结点左子树的深度
+        int right = maxDepth(root.right); //递归右子树,返回当前结点右子树的深度
+        return Math.max(left, right) + 1;
+    }
 
     //方法二：层序遍历
-    public int maxDepth(TreeNode root) {
+  /*  public int maxDepth(TreeNode root) {
         if (root == null) return 0;
         int depth = 0;  //树的深度
         int levelSize = 1;  //存储每一层结点数量
@@ -35,5 +37,5 @@ public class _104_二叉树的最大深度 {
             }
         }
         return depth;
-    }
+    }*/
 }
