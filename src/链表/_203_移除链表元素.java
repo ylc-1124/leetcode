807f5006e0df5e6e1544491bd57e19d28ec31f04
ylc-1124 <853703920@qq.com>
2021-08-09 13:10:20 +0800

    打卡

--- src/链表/_203_移除链表元素.java
+++ src/链表/_203_移除链表元素.java
@@ -5,16 +5,18 @@ package 链表;
  */
 public class _203_移除链表元素 {
     public ListNode removeElements(ListNode head, int val) {
-        if (head==null) return head;
-        ListNode visualHead = new ListNode(0, head);
-        ListNode cur = visualHead;
-        while (cur.next != null) {
-            if (cur.next.val == val) {
-                cur.next = cur.next.next;
-            } else {   //必须用else不然会多走，少比较
-                cur = cur.next;
+        if (head == null) {
+            return null;
+        }
+        ListNode newHead = new ListNode(0, head);
+        ListNode prev = newHead;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
             }
         }
-        return visualHead.next;
+        return newHead.next;
     }
 }
