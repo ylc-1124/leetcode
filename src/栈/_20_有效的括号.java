ebd59d398f4ea1472d735c16339d85b97b235a65
ylc-1124 <853703920@qq.com>
2021-08-09 16:15:23 +0800

    打卡

--- src/栈/_20_有效的括号.java
+++ src/栈/_20_有效的括号.java
@@ -1,5 +1,7 @@
 package 栈;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -7,20 +9,43 @@ import java.util.Stack;
  */
 public class _20_有效的括号 {
     public boolean isValid(String s) {
+        //先写个对应映射关系，也叫字典，这样写代码简洁
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            //左括号压栈
-            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-                stack.push(s.charAt(i));
-            } else {  //右括号弹栈
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                //是右括号
                 if (stack.isEmpty()) return false;
-                Character c = stack.pop();
-                if (c=='(' && s.charAt(i)!=')') return false;
-                if (c=='[' && s.charAt(i)!=']') return false;
-                if (c=='{' && s.charAt(i)!='}') return false;
+                if (c!=map.get(stack.pop())) return false;
             }
         }
-
         return stack.isEmpty();
     }
+  /*  public boolean isValid(String s) { //方法一
+        Stack<Character> stack = new Stack<>();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                //遇到右括号
+                char left = stack.pop();
+                if (left == '(' && c != ')') return false;
+                if (left == '[' && c != ']') return false;
+                if (left == '{' && c != '}') return false;
+            }
+        }
+        return stack.isEmpty();
+    }*/
 }
