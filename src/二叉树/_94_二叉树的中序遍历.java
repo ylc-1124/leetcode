c563b5fed2b34a70a65ca83094cd23e1ed583ec3
ylc-1124 <853703920@qq.com>
2021-08-29 12:06:49 +0800

    通过

--- src/二叉树/_94_二叉树的中序遍历.java
+++ src/二叉树/_94_二叉树的中序遍历.java
@@ -10,16 +10,15 @@ import java.util.List;
 public class _94_二叉树的中序遍历 {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        inorder(root,list);
+        inorder(root, list);
         return list;
     }
 
-    private void inorder(TreeNode root, List<Integer> list) {
-        if (root == null) {
-            return;
-        }
-        inorder(root.left, list);
-        list.add(root.val);
-        inorder(root.right,list);
+    private void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        inorder(node.left, list);
+        //...
+        list.add(node.val);
+        inorder(node.right, list);
     }
 }
--- src/链表/_876_链表的中间结点.java
+++ src/链表/_876_链表的中间结点.java
@@ -31,20 +31,5 @@ public class _876_链表的中间结点 {
         }
         return slow;
     }
-    public static void main(String[] args) {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(6);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node6.next = null;
-        ListNode listNode = new _876_链表的中间结点().middleNode(node1);
-        System.out.println(listNode.val);
-    }
+
 }
