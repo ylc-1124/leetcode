98bf053060918de2abe946b980640d8b12135c57
ylc-1124 <853703920@qq.com>
2021-08-30 18:35:57 +0800

    改个方法名

--- src/二叉树/_108_将有序数组转换为二叉搜索树.java
+++ src/二叉树/_108_将有序数组转换为二叉搜索树.java
@@ -6,17 +6,17 @@ package 二叉树;
 public class _108_将有序数组转换为二叉搜索树 {
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums == null || nums.length == 0) return null;
-        return sortedArrayToBST(nums, 0, nums.length - 1);
+        return dfs(nums, 0, nums.length - 1);
     }
 
-    private TreeNode sortedArrayToBST(int[] nums, int begin, int end) {
+    private TreeNode dfs(int[] nums, int begin, int end) {
         if (begin > end) return null;
         /*造根节点*/
         int mid = (begin + end) / 2;
         TreeNode root = new TreeNode(nums[mid]);
 
-        TreeNode left = sortedArrayToBST(nums, begin, mid - 1);
-        TreeNode right = sortedArrayToBST(nums, mid + 1, end);
+        TreeNode left = dfs(nums, begin, mid - 1);
+        TreeNode right = dfs(nums, mid + 1, end);
 
         root.left = left;
         root.right = right;
