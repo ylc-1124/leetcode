885ab8efb8a50c255d15644379807db5b6fbdbe3
ylc-1124 <853703920@qq.com>
2021-08-31 12:34:18 +0800

    通过

--- src/二叉树/_110_平衡二叉树.java
+++ src/二叉树/_110_平衡二叉树.java
@@ -5,16 +5,15 @@ package 二叉树;
  */
 public class _110_平衡二叉树 {
     public boolean isBalanced(TreeNode root) {
-        if (root==null) return true;
-        return Math.abs(height(root.left) - height(root.right))<=1
+        if (root == null) return true;
+        return Math.abs(height(root.left) - height(root.right)) <= 1
                 && isBalanced(root.left)
                 && isBalanced(root.right);
     }
 
-    private int height(TreeNode node) {
-        if (node==null) return 0;
-        int left = height(node.left);  //左子树深度
-        int right = height(node.right); //右子树深度
-        return Math.max(left, right) + 1;
+    private int height(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(height(root.left), height(root.right)) + 1;
     }
+
 }
---
+++ src/二叉树/_538_把二叉搜索树转换为累加树.java
@@ -0,0 +1,24 @@
+package 二叉树;
+
+/**
+ * https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+ */
+public class _538_把二叉搜索树转换为累加树 {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+    /**
+     * 将以root为根节点的树的val设置为比他大的所有结点的和
+     */
+    private void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.right);
+        /*中序遍历代码*/
+        sum += root.val;
+        root.val = sum;
+        dfs(root.left);
+    }
+
+}
