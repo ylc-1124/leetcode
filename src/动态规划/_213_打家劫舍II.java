e76b0fa1f0aecdfeb39ac44e020486494c3503a0
ylc-1124 <853703920@qq.com>
2021-09-11 17:12:27 +0800

    打家劫舍系列

--- src/动态规划/_213_打家劫舍II.java
+++ src/动态规划/_213_打家劫舍II.java
@@ -1,10 +1,42 @@
 package 动态规划;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode-cn.com/problems/house-robber-ii/
  */
 public class _213_打家劫舍II {
+
+    int[] memo;
     public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        memo = new int[nums.length];
+        Arrays.fill(memo, -666);
+        /*由题意：第一家住户和最后一家住户最多只能偷其中一家
+         * 考虑两种情况：1、第一家包含在可选择是否偷盗的范围中
+         *            2、第一家排除在外，最后一家包含在可选择是否偷盗的范围中
+         * ps：这两种情况中其实已经覆盖了这两家都不偷盗的情况了*/
+        int notInvolveLast = dp(nums, nums.length - 2, 0);
+        Arrays.fill(memo, -666); //因为两次递归共用的一个备忘录，所以要清空一下
+
+        int notInvolveFirst = dp(nums, nums.length - 1, 1);
+        return Math.max(notInvolveLast, notInvolveFirst);
+    }
+
+    private int dp(int[] nums, int cur, int begin) {
+        if (cur < begin) return 0;
+        //0、查备忘录
+        if (memo[cur] != -666) return memo[cur];
+        //1、选择偷当前住户(前提没有偷他前一家)
+        int doIt = nums[cur] + dp(nums, cur - 2, begin);
+        //2、选择不偷当前住户
+        int notDoIt = dp(nums, cur - 1, begin);
+        memo[cur] = Math.max(doIt, notDoIt);
+        return memo[cur];
+    }
+
+
+   /* public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
         return Math.max(
                 robRange(nums, 1, nums.length - 1),//最后一家不考虑偷的情况
@@ -20,5 +52,5 @@ public class _213_打家劫舍II {
                     nums[i - 1] + dp[i - 2]);
         }
         return dp[end];
-    }
+    }*/
 }
---
+++ src/动态规划/_337_打家劫舍III.java
@@ -0,0 +1,31 @@
+package 动态规划;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://leetcode-cn.com/problems/house-robber-iii/
+ */
+public class _337_打家劫舍III {
+    Map<TreeNode, Integer> memo = new HashMap<>();
+    public int rob(TreeNode root) {
+        if (root == null) return 0;
+        if (memo.containsKey(root)) {
+            return memo.get(root);
+        }
+        //抢当前节点
+        int doIt = root.val
+                + (root.left == null ? 0 : rob(root.left.left) + rob(root.left.right))
+                + (root.right == null ? 0 : rob(root.right.left) + rob(root.right.right));
+
+        //不抢当前节点
+        int notDoIt = rob(root.left) + rob(root.right);
+
+        int maxMoney = Math.max(doIt, notDoIt);
+        memo.put(root, maxMoney);
+
+        return maxMoney;
+    }
+}
