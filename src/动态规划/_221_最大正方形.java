203c83155a41e050d5f3bb3268e90522e6a181d1
ylc-1124 <853703920@qq.com>
2023-10-17 23:21:58 +0800

    ac

---
+++ src/动态规划/_221_最大正方形.java
@@ -0,0 +1,43 @@
+package 动态规划;
+
+public class _221_最大正方形 {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // 以 matrix[i][j]作为右下角元素的最大正方形变长
+        int[][] dp = new int[m][n];
+        // base case
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == '1') {
+                dp[0][j] = 1;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == '1') {
+                dp[i][0] = 1;
+            }
+        }
+
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    int min = Math.min(dp[i - 1][j - 1], dp[i - 1][j]);
+                    min = Math.min(min, dp[i][j - 1]);
+                    dp[i][j] = min + 1;
+                }
+            }
+        }
+
+        // 找结果
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max * max;
+    }
+
+
+}
---
+++ src/每日一题/_2652_倍数求和.java
@@ -0,0 +1,13 @@
+package 每日一题;
+
+public class _2652_倍数求和 {
+    public int sumOfMultiples(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                res += i;
+            }
+        }
+        return res;
+    }
+}
